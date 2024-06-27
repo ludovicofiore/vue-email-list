4934322d709f5e92a87987f88a007a5f5d7e883a
ludoff <ludovicofiore25@gmail.com>
2024-06-27 15:38:43 +0200

    feat: add cycle to generate multiple emails and output in page

--- js/main.js
+++ js/main.js
@@ -11,15 +11,20 @@ createApp({
     },
     mounted () {
 
-        // chiamo api per creare email random
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then((emailGenerated) => {
-            const result = emailGenerated.data.response;
-            console.log(result);
+        // creo ciclo per generare più email
+        for (let i = 0; i < 10; i++) {
+            
+            // chiamo api per creare email random
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((emailGenerated) => {
+                const result = emailGenerated.data.response;
+                console.log(result);
 
-            // pusho email generata in array
-            this.emailList.push(result);
-            console.log(this.emailList);
-        })
+                // pusho email generata in array
+                this.emailList.push(result);
+                console.log(this.emailList);
+            })
+        }
+        
     }
 }).mount("#app");
