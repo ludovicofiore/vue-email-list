1024c8f5878fadf224da4d8ed533be552177ff91
ludoff <ludovicofiore25@gmail.com>
2024-06-27 15:30:16 +0200

    feat: add api to generate random email and push it into array

--- js/main.js
+++ js/main.js
@@ -4,7 +4,22 @@ const { createApp } = Vue;
 createApp({
     data() {
         return{
+            // array dove pushare email generate
+            emailList: [],
 
         }
     },
+    mounted () {
+
+        // chiamo api per creare email random
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((emailGenerated) => {
+            const result = emailGenerated.data.response;
+            console.log(result);
+
+            // pusho email generata in array
+            this.emailList.push(result);
+            console.log(this.emailList);
+        })
+    }
 }).mount("#app");
